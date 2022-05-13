40a6aa857771c5d89f1b6ee1c0f40282f5f2425e
JamosW <104772245+JamosW@users.noreply.github.com>
2022-05-12 23:52:06 -0400

    added treemapsection

--- agri_data.R
+++ agri_data.R
@@ -11,6 +11,8 @@ cleaned <- function(x) {
   return(l)
 }
 
+cleaned()
+
 
 #take geometry from world sf object, get the X and Y Coordinates and append the columns
 worldXY <- rnaturalearthdata::countries50 |>
@@ -40,7 +42,7 @@ mainplot <- function(data = joined, type = "Area harvested") {
 mapData <- function(data, countries = NULL, commodities = NULL, years = NULL){
   
   ndata <- data |> 
-    select(Area, Item, Element, years, X,Y)
+    select.(Area, Item, Element, years, X,Y)
   
   ndata$Item <- cleaned()$item
   ndata$Area <- cleaned()$area
@@ -62,25 +64,25 @@ mapData <- function(data, countries = NULL, commodities = NULL, years = NULL){
 
 
 
-#######################################################Plot Data ##########################################################
+##################################################Line Plot Data ##########################################################
 
 
 linePlot <- function(data) {
   
   #lineplot
-  lp <- function(x, group) 
+  lp <- function(x, group, clr) 
   {
     ggplot(x) + 
       geom_smooth(aes(x = name, y = value, group = group, color = group), size = 1, na.rm = TRUE, se = FALSE) +
       scale_y_log10() + labs(x = "Year",
                              y = "Production",
-                             color = "Commodity") + theme_bw()
+                             color = clr) + theme_bw()
     }
   
   #only pivot columns that are years
   pivot <- function(x)
   {
-    select(x, Area, Item, Element, contains("Y")) |>  
+    select.(x, Area, Item, Element, contains("Y")) |>  
       pivot_longer.(contains("Y")) |> 
       filter.(value != "A") |> 
       mutate.(value = as.numeric(value), name = str_remove(name, c("Y")) |> as.numeric())
@@ -92,46 +94,64 @@ linePlot <- function(data) {
       filter.(Area %in% c("Africa", "Europe", "Asia", "Americas", "Oceania"), 
               Element == "Area harvested") |>
       pivot() %>% 
-      lp(.$Area)
+      lp(.$Area, data$Area)
   } else if(all(data$Area == "Africa")) {
    p <-  joined |> 
       filter.(Area %in% c("Northern Africa", "Southern Africa", "Western Africa"), 
               Element == "Area harvested") |>
       pivot() %>% 
-      lp(.$Area)
+      lp(.$Area, data$Area )
   } else if(all(data$Area == "Asia")) {
     p <-  joined |> 
       filter.(Area %in% c("Central Asia", "Southern Asia", "Western Asia"), 
               Element == "Area harvested") |>
       pivot() %>% 
-      lp(.$Area)
+      lp(.$Area, data$Area)
   } else if(all(data$Area == "Americas")) {
     p <- joined |> 
       filter.(Area %in% c("Northern America", "Central America", "South America"), 
               Element == "Area harvested") |>
       pivot() %>% 
-      lp(.$Area)
+      lp(.$Area, data$Area)
   } else if (all(data$Area == "Europe")) {
     p <- joined |> 
       filter.(Area %in% c("Northern Europe", "Southern Europe", "Western Europe", "Eastern Europe"), 
               Element == "Area harvested") |>
       pivot() %>% 
-      lp(.$Area)
+      lp(.$Area, data$Area)
     #only when on area is selected and less than 10 items, more than 10 is too much clutter
-  } else if((data$Item |> unique() |> length()) <= 10 & (data$Area |> unique() |> length()) == 1){
-    p <- joined |> 
-      filter.(Area %in% Area, Element == "Area harvested", Item %in% data$Item) |>
-      pivot() %>% 
-      lp(.$Item)
-  } else if( (data$Item |> unique() |> length()) <= 10 & between.((data$Area |> unique() |> length()), 2, 10) ){
+  } else if( between.((data$Item |> unique() |> length()), 1,12) & between.((data$Area |> unique() |> length()), 2, 12)){
     p <- joined |> 
       filter.(Area %in% data$Area, Element == "Area harvested", Item %in% data$Item) |>
       pivot() %>% 
-      lp(.$Item) + facet_wrap(vars(Area))
+      lp(.$Item, "Commodity") + facet_wrap(vars(Area))
+  } else if((data$Item |> unique() |> length()) > 10 & (data$Area |> unique() |> length()) <= 12){
+    p <- joined |> 
+      filter.(Area %in% data$Area, Element == "Area harvested", Item %in% data$Item) |>
+      pivot() %>% 
+      lp(.$Area, "Country") + facet_wrap(vars(Area))
+  } else if( between.((data$Item |> unique() |> length()), 1,12) & (data$Area |> unique() |> length()) > 12 ){
+    p <- joined |> 
+      filter.(Area %in% data$Area, Element == "Area harvested", Item %in% data$Item) |>
+      pivot() %>% 
+      lp(.$Item, "Commodity") + facet_wrap(vars(Item))
   }
     
    return(p)
 }
 
+####################################################Tree Map###############################################################
+library(treemapify)
+#library(ggplot2)
 
+p <- ggplot(G20, aes(area = gdp_mil_usd, fill = region)) +
+  geom_treemap()
+
+p
+
+(dat <- mapData(joined, commodities = "Apricots", years = paste0("Y", 2020)))
+
+ggplot(data = dat, aes(area = Y2020, fill = Area)) + 
+  geom_treemap(show.legend = FALSE, na.rm = TRUE, start = "topleft") +
+  scale_fill_brewer(palette = "YlOrRd")
 
